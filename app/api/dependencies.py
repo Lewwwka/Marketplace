a680e7c437e512bf2950f983543d23c26d773284
Alexey <lewwwwwka@gmail.com>
2026-02-17 21:59:23 +0300

    create jwt access

---
+++ app/api/dependencies.py
@@ -0,0 +1,31 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+from db.models import User
+from db.database import get_db
+from core.security import verify_token
+from core.exceptions import ForbiddenException
+
+security = HTTPBearer()
+
+
+async def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security), db=Depends(get_db)
+) -> User:
+    email = await verify_token(credentials.credentials)
+    if email is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверные учетные данные",
+        )
+
+    user = db.query(User).filter(User.email == email).first()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Пользователь не найден"
+        )
+
+    if not user.is_active:
+        raise ForbiddenException()
+
+    return user
