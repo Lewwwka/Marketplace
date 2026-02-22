7abe53c538e6912ca8a8636c24106fd6c2cfc3bd
Alexey <lewwwwwka@gmail.com>
2026-02-22 21:14:45 +0300

    create tests fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,61 @@
+import asyncio
+
+import pytest
+import pytest_asyncio
+from httpx import AsyncClient, ASGITransport
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from app.main import app
+from app.db.database import get_db
+from app.db.models import Base
+from app.core.config import settings
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def setup_db_once():
+
+    engine = create_async_engine(settings.DATABASE_ASYNC_URL)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+    await engine.dispose()
+
+
+@pytest_asyncio.fixture
+async def db_session():
+
+    engine = create_async_engine(settings.DATABASE_ASYNC_URL)
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+    async with engine.begin() as conn:
+        session = async_session(bind=conn)
+        try:
+            yield session
+        finally:
+            await session.close()
+            await engine.dispose()
+
+
+@pytest_asyncio.fixture
+async def client(db_session):
+
+    async def override_get_db():
+        yield db_session
+
+    app.dependency_overrides[get_db] = override_get_db
+
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        yield ac
+
+    app.dependency_overrides.clear()
